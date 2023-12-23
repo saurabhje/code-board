f051541bb68cc65ccf3c39455c0951743053d973
saurabhje <work.saurabh@outlook.com>
2023-12-23 09:48:47 -0800

    Add: Data Fetch(Static Rendering) and Send

--- app/static/page.tsx
+++ app/static/page.tsx
@@ -1,18 +1,27 @@
-import { GetServerSideProps } from "next";
 import supabase from "../../utils/supabase/client";
 
 export default async function Snippets() {
-  const {data: snippets, error } = await supabase
+  const { data: snippets, error } = await supabase
     .from("snippets")
     .select("title,content");
+
+  if (error) {
+    return (
+      <div className="mt-10 p-10">
+        <p>Error Loading data</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-6">
-      {snippets.map((snippet, index) => (
-        <div key={index}>
-          <h2>{snippet.title}</h2>
-          <pre>{snippet.content}</pre>
-        </div>
-      ))}
+      {snippets &&
+        snippets.map((snippet, index) => (
+          <div key={index}>
+            <h2>{snippet.title}</h2>
+            <pre>{snippet.content}</pre>
+          </div>
+        ))}
     </div>
   );
 }
