4b8ee7771b5b98973e6f82bf4c68cd1f22e22537
saurabhje <work.saurabh@outlook.com>
2023-12-23 08:51:20 -0800

    Style Form

--- app/new-code/page.tsx
+++ app/new-code/page.tsx
@@ -3,7 +3,7 @@ import styles from "../utils.module.css";
 export default function AddCode() {
   return (
     <div className="p-10 w-full">
-      <form>
+      <form className={`${styles.form}`}>
         <div className="w-full flex flex-col">
           <label>
             Title
@@ -11,20 +11,20 @@ export default function AddCode() {
           <input
             type="text"
             placeholder=" "
-            className="border-white bg-btn-background px-2 py-1 focus:outline-none"
+            className="bg-btn-background px-2 py-1 focus:outline-none focus:border-white"
           />
         </div>
         <div className="w-full flex flex-col mt-4">
           <label htmlFor="title">
             Language{" "}
           </label>
-          <input id="title" type="text" className="text-black px-2 py-1 focus:outline-none" />
+          <input id="title" type="text" className="bg-btn-background px-2 py-1 focus:outline-none focus:border-white" />
         </div>
         <div className="w-full flex flex-col mt-4">
           <label htmlFor="snippet">
             Your code
           </label>
-          <textarea id="snippet" className="text-black px-2 py-1 focus:outline-none" rows={8} />
+          <textarea id="snippet" className="bg-btn-background px-2 py-1 focus:outline-none focus:border-white" rows={8} />
         </div>
         <button  type="submit" className="mt-4 py-1 px-5 rounded-md bg-btn-background md:hover:bg-btn-background-hover">Add</button>
       </form>
