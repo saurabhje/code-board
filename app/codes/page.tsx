c809e593b520fbac2d2a369737686df51b0a5b91
saurabhje <work.saurabh@outlook.com>
2023-12-29 22:57:53 +0530

    styling Code

--- app/codes/page.tsx
+++ app/codes/page.tsx
@@ -1,11 +1,12 @@
 import Snippets from "@/components/ShowCode";
 export default function Jovier(){
     return(
-        <div className="w-full py-20 px-4 lg:px-1">
-            <div className="flex">
-                <input className="bg-btn-background px-2 py-1 focus:outline-none focus:border-white" type="text" />
-                <button className="px-2 py-1 rounded bg-btn-background">Search</button>
+        <div className="w-full py-12 px-4 lg:px-1">
+            <div className="flex flex-col">
+                <label htmlFor="code-syntax">Search Syntax</label>
+                <input id="code-syntax" className="border border-zinc-500 p-1 w-full lg:w-2/3 bg-transparent focus:outline-none focus:border-white" type="text" />
             </div>
+                <button className="mt-2 py-1 px-5 rounded-md bg-btn-background md:hover:bg-btn-background-hover">Search</button>
             <Snippets/>
         </div>
     )
