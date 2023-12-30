32c210af88bd9d83bb3666c9c96db0553498e3c1
saurabhje <work.saurabh@outlook.com>
2023-12-31 02:24:14 +0530

    Style Home Hero Sec

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,38 +1,30 @@
-"use client";
+"use client"
+import React, { useState } from "react";
 import Link from "next/link";
-import { useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
-  let code1 = `#include<stdc++.h/bits>
+  const code1 = `#include<stdc++.h/bits>
   using namespace std;
   int main(){
       string str = "Hello World!";
       cout<<str<<endl;
       return 0;
   }`;
-  let code2 = `
-  const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
 
-  fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(\`HTTP error! Status: \${response.status}\`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
+  const code2 = `function fibonacci(n) {
+    if (n <= 1) {
+      return n;
+    }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  const result = fibonacci(10);
+  console.log("Fibonacci(10):", result);
+  
 `;
 
-
-  const [code, setCode] = useState(code1);
-  const [random, setRandom] = useState('initial')
-
+  const [random, setRandom] = useState(">..");
 
   return (
     <div>
@@ -40,16 +32,16 @@ export default function Hero() {
         <div className="w-full lg:w-3/4 border rounded bg-inherit py-1 px-2">
           <TypeAnimation
             sequence={[
-              "How to print Hello World! in C++",
+              "How to print Hello World! in C++", 
               () => {
-                setRandom('after first');
+                setRandom("cpp");
               },
               6000,
-              "How to fetch data in javascript",
-              6000,
+              "Fibonacci number using in JavaScript",
               () => {
-                setRandom('After Second');
+                setRandom("javascript");
               },
+              6000,
             ]}
             wrapper="span"
             cursor={true}
@@ -64,13 +56,24 @@ export default function Hero() {
           Start searching
         </Link>
       </div>
-        {random}
-      <div className="h-fit bg-zinc-700 animate-in">
-        <p className="py-1 px-2 bg-zinc-900">cpp</p>
-        <TypeAnimation 
-        className="min-h-[120px] px-2 py-1 whitespace-pre block"
-        sequence={[`${code}`,5000]}
-        />
+      <div className="min-h-[220px] bg-slate-900 animate-in">
+        <p className="py-1 px-2 bg-zinc-900">{random}</p>
+        {random == "javascript" && (
+          <TypeAnimation
+            className="min-h-[120px] px-2 py-1 whitespace-pre block font-mono"
+            speed={99}
+            sequence={[`${code2}`, 6000]}
+            cursor={false}
+          />
+        )}
+        {random == "cpp" && (
+          <TypeAnimation
+            className="px-2 py-1 whitespace-pre block font-mono"
+            speed={99}
+            sequence={[`${code1}`, 6000]}
+            cursor={false}
+          />
+        )}
       </div>
     </div>
   );
