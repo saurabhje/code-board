1921cfce0fd11b761cd13ca0182b2838f17f581c
saurabhje <work.saurabh@outlook.com>
2023-12-31 02:52:53 +0530

    Add search

--- app/codes/page.tsx
+++ app/codes/page.tsx
@@ -1,13 +1,33 @@
+import { getSearchData } from "@/components/ServerData";
 import Snippets from "@/components/ShowCode";
-export default function Jovier(){
-    return(
-        <div className="w-full py-12 px-4 lg:px-1">
-            <div className="flex flex-col">
-                <label htmlFor="code-syntax">Search Syntax</label>
-                <input id="code-syntax" className="border border-zinc-500 p-1 w-full lg:w-2/3 bg-transparent focus:outline-none focus:border-white" type="text" />
-            </div>
-                <button className="mt-2 py-1 px-5 rounded-md bg-btn-background md:hover:bg-btn-background-hover">Search</button>
-            <Snippets/>
+import Code from "@/components/Code";
+
+interface Searchdata {
+    title: string;
+    language: string;
+    content: string;
+  }
+export default async function Jovier() {
+    const searchQuery = 'binary search';
+  const Searchdata = (await getSearchData(searchQuery)) as Searchdata[];
+  return (
+    <div className="w-full py-12 px-4 lg:px-1">
+      <div className="flex flex-col">
+        <label htmlFor="code-syntax">Search Syntax</label>
+        <input
+          id="code-syntax"
+          className="border border-zinc-500 p-1 w-full lg:w-2/3 bg-transparent focus:outline-none focus:border-white"
+          type="text"
+        />
+      </div>
+      <button className="mt-2 py-1 px-5 rounded-md bg-btn-background md:hover:bg-btn-background-hover">Search</button>
+      {Searchdata.map((searchItem, index) => (
+        <div key={index} className="break-inside-avoid">
+          <h2 className="mb-2 text-xl">{searchItem.title}</h2>
+          <Code language={searchItem.language} code={searchItem.content} />
         </div>
-    )
+      ))}
+      {/* <Snippets /> */}
+    </div>
+  );
 }
--- components/ServerData.tsx
+++ components/ServerData.tsx
@@ -1,11 +1,25 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
-export default async function getServerData() {
+export async function getServerData() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const api = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   const supabaseServer = new SupabaseClient(url, api);
 
   const { data, error } = await supabaseServer.from('snippets').select('title, language, content');
+
+  if (error) {
+    return error;
+  }
+
+  return data;
+}
+
+export async function getSearchData(arg:string) {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const api = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabaseServer = new SupabaseClient(url, api);
+
+  const { data, error } = await supabaseServer.from('snippets').select().textSearch('title',`${arg}`);
 
   if (error) {
     return error;
