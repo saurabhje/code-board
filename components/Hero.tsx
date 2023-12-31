7b3497bc321e300b66d179b44555a5beaeb60ef1
saurabhje <work.saurabh@outlook.com>
2023-12-31 11:54:12 +0530

    Style Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
     if (n <= 1) {
       return n;
     }
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  return fibonacci(n-1) + fibonacci(n-2);
   }
 
   const result = fibonacci(10);
